92d626cf21d0c1f68d4d09e77570e6d29716e20c
ErwanDuprey <erwan.duprey@free.fr>
2023-06-12 19:45:59 +0200

    Secret 1 for dynamic_programming.py done

--- algorithms/dynamic_programming.py
+++ algorithms/dynamic_programming.py
@@ -231,37 +231,116 @@ def value_iteration_on_grid_world(env: SingleAgentGridWorldEnv,
     return PolicyAndValueFunction(pi=dict(enumerate(pi)), v=dict(enumerate(V.flatten(), 1)))
 
 
-def policy_evaluation_on_secret_env1() -> ValueFunction:
+def policy_evaluation_on_secret_env1(theta: float = 0.0000001) -> ValueFunction:
     """
     Creates a Secret Env1
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
     Returns the Value function (V(s)) of this policy
     """
     env = Env1()
-    # TODO
-    pass
+    V = np.zeros(len((env.states())))
+    while True:
+        delta = 0.0
+        for s in range(len(env.states())):
+            old_v = V[s]
+            total = 0.0
+            for a in env.actions():
+                total_inter = 0.0
+                for s_p in range(len(env.states())):
+                    for r in range(len(env.rewards())):
+                        total_inter += env.transition_probability(s, a, s_p, r) * (env.rewards()[r] + 0.99999 * V[s_p])
+                total_inter = (1/len(env.actions())) * total_inter
+                total += total_inter
+            V[s] = total
+            delta = max(delta, np.abs(V[s] - old_v))
+        if delta < theta:
+            return dict(enumerate(V.flatten(), 1))
 
 
-def policy_iteration_on_secret_env1() -> PolicyAndValueFunction:
+def policy_iteration_on_secret_env1(theta: float = 0.0000001) -> PolicyAndValueFunction:
     """
     Creates a Secret Env1
     Launches a Policy Iteration Algorithm in order to find the Optimal Policy and its Value Function
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
-    # TODO
-    pass
+    V = np.zeros(len((env.states())))
+    pi = np.random.randint(0, len(env.actions()), len(env.states()))
+
+    while True:
+        # policy evaluation
+        while True:
+            delta = 0
+            for s in range(len(env.states())):
+                old_v = V[s]
+
+                total = 0.0
+
+                for s_p in range(len(env.states())):
+                    for r in range(len(env.rewards())):
+                        total += env.transition_probability(s, pi[s], s_p, r) * (env.rewards()[r] + 0.99999999 * V[s_p])
+                V[s] = total
+                delta = max(delta, np.abs(old_v - V[s]))
+            if delta < theta:
+                break
+
+        # policy improvement
+        policy_stable = True
+        for s in range(len(env.states())):
+            old_action = pi[s]
+
+            best_a = None
+            best_action_score = None
+            for a in env.actions():
+                total = 0.0
+                for s_p in range(len(env.states())):
+                    for r in range(len(env.rewards())):
+                        total += env.transition_probability(s, a, s_p, r) * (env.rewards()[r] + 0.999999 * V[s_p])
+                if best_a is None or total > best_action_score:
+                    best_a = a
+                    best_action_score = total
+
+            pi[s] = best_a
+            if best_a != old_action:
+                policy_stable = False
+
+        if policy_stable:
+            return PolicyAndValueFunction(pi=dict(enumerate(pi)), v=dict(enumerate(V.flatten(), 1)))
 
 
-def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
+def value_iteration_on_secret_env1(theta: float = 0.0000001) -> PolicyAndValueFunction:
     """
     Creates a Secret Env1
     Launches a Value Iteration Algorithm in order to find the Optimal Policy and its Value Function
     Prints the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
-    # TODO
-    pass
+    V = np.zeros(len(env.states()))
+    pi = np.random.randint(0, len(env.actions()), len(env.states()))
+    while True:
+        delta = 0
+        for s in range(len(env.states())):
+            old_v = V[s]
+
+            best_action_score = None
+            best_a = None
+            for a in env.actions():
+                total = 0.0
+                for s_p in range(len(env.states())):
+                    for r in range(len(env.rewards())):
+                        total += env.transition_probability(s, a, s_p, r) * (env.rewards()[r] + 0.99999999 * V[s_p])
+
+                if best_action_score is None or total > best_action_score:
+                    best_action_score = total
+                    best_a = a
+
+            V[s] = best_action_score
+            pi[s] = best_a
+            delta = max(delta, np.abs(old_v - V[s]))
+        if delta < theta:
+            break
+
+    return PolicyAndValueFunction(pi=dict(enumerate(pi)), v=dict(enumerate(V.flatten(), 1)))
 
 
 def demo():
@@ -278,3 +357,5 @@ def demo():
     print(policy_evaluation_on_secret_env1())
     print(policy_iteration_on_secret_env1())
     print(value_iteration_on_secret_env1())
+
+    print("-----------------")
