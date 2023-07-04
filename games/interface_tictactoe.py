a95516f316b94f1166cf3db6416d4a991e6fd7f4
tomcareghi <tom.careghi@regid-dgac.net>
2023-07-04 18:47:06 +0200

    interface tictactoe etape 1 cleen

--- games/interface_tictactoe.py
+++ games/interface_tictactoe.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from games.tictactoe import TicTacToe
 
-# Configuration de base de la fenêtre
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 BUTTON_WIDTH = 500
@@ -19,7 +18,6 @@ CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
 SPACE = 55
 
-# Couleurs RGB
 LIGHT_BLUE = (14, 41, 84)
 DARK_BLUE = (31, 110, 140)
 ORANGE = (132, 167, 161)
@@ -27,15 +25,12 @@ ORANGE = (132, 167, 161)
 BUTTON_COLOR = DARK_BLUE
 WHITE = (242, 234, 211)
 
-# Crée une surface de fenêtre
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("TicTacToe")
 
 def draw_lines():
-    # Dessine les lignes horizontales
     pygame.draw.line(screen, WHITE, (0, 200), (600, 200), LINE_WIDTH)
     pygame.draw.line(screen, WHITE, (0, 400), (600, 400), LINE_WIDTH)
-    # Dessine les lignes verticales
     pygame.draw.line(screen, WHITE, (200, 0), (200, 600), LINE_WIDTH)
     pygame.draw.line(screen, WHITE, (400, 0), (400, 600), LINE_WIDTH)
 
@@ -62,11 +57,11 @@ def bouble_button(name1, name2):
                 BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_COLOR)
 
 def draw_game_over(winer):
-    font = pygame.font.Font(None, 74)  # Choisissez la taille de la police ici.
+    font = pygame.font.Font(None, 74)
     if winer == -1:
-        text = font.render('Circle players won!', True, WHITE)  # Affiche 'Gagné' si le joueur a gagné.
+        text = font.render('Circle player won!', True, WHITE)
     else:
-        text = font.render('Cross players won!', True, WHITE)  # Affiche 'Game Over' si le joueur a perdu.
+        text = font.render('Cross player won!', True, WHITE)
     text_rect = text.get_rect()
     text_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 4)
     window.blit(text, text_rect)
@@ -89,23 +84,17 @@ def play_game():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouseX = event.pos[0]  # Coordonnées en X
-                mouseY = event.pos[1]  # Coordonnées en Y
+                mouseX = event.pos[0]
+                mouseY = event.pos[1]
                 clicked_row = int(mouseY // 200)
                 clicked_col = int(mouseX // 200)
                 if env.board[clicked_row][clicked_col] == 0:
                     env.play((clicked_row, clicked_col))
-                    # if env.is_game_over():
-                    #     restart(env)
-                    # draw_figures(env)
 
         screen.fill(LIGHT_BLUE)
         if not env.is_game_over():
             draw_figures(env)
             draw_lines()
-
-            # draw_agent(env.state_id())
-            # draw_move_count(move_count)
         else:
             draw_game_over(env.player)
             bouble_button("exit", "rejouer")
