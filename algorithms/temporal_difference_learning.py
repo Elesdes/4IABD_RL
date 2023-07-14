3fe238c50a82996cdc64df2f9c50ac8c3b8f89e0
ErwanDuprey <erwan.duprey@free.fr>
2023-07-14 16:55:21 +0200

    expected sarsa tictactoe done?

--- algorithms/temporal_difference_learning.py
+++ algorithms/temporal_difference_learning.py
@@ -467,15 +467,73 @@ def q_learning_on_tic_tac_toe_solo(
     return PolicyAndActionValueFunction(pi=dict(enumerate(pi)), q=dict(enumerate(Q)))
 
 
-def expected_sarsa_on_tic_tac_toe_solo() -> PolicyAndActionValueFunction:
+def expected_sarsa_on_tic_tac_toe_solo(
+    env: TicTacToe,
+    gamma: float = 0.9999,
+    alpha: float = 0.01,
+    epsilon: float = 0.2,
+    max_episodes_count: int = 10000,
+) -> PolicyAndActionValueFunction:
     """
     Creates a TicTacToe Solo environment (Single player versus Uniform Random Opponent)
     Launches a Expected SARSA Algorithm in order to find the optimal epsilon-greedy Policy and its action-value function
     Returns the optimal epsilon-greedy Policy and its Action-Value function (Q(s,a))
     Experiment with different values of hyper parameters and choose the most appropriate combination
     """
-    # TODO
-    pass
+    assert epsilon > 0
+    assert alpha > 0
+    pi = np.zeros((2, 9))
+    Q = np.random.uniform(-1.0, 1.0, (2, 9))
+
+    for ep_id in range(max_episodes_count):
+        env.reset()
+        while not env.is_game_over():
+            if env.player == -1:
+                s = 0
+            else:
+                s = env.player
+            aa = env.available_actions()
+
+            if np.random.random() < epsilon:
+                # a = np.random.choice(aa)
+                a = aa[np.random.randint(aa.shape[0], size=1), :][0]
+            else:
+                # print(np.argmax(Q[s][aa]), " ", Q[s] , " ", s, " ", aa, " ", [case[0] * 3 + case[1] for case in aa], " ",env.is_game_over(), "\n------")
+                best_a_idx = np.argmax(Q[s][[case[0] * 3 + case[1] for case in aa]])
+                a = (np.array(((0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2))))[best_a_idx]
+            if env.is_game_over():
+                old_score = -env.player
+                env.play(a)
+                new_score = -env.player
+            else:
+                old_score = 0
+                env.play(a)
+                if env.is_game_over():
+                    new_score = env.player
+                else:
+                    new_score = 0
+            r = new_score - old_score
+
+            if env.player == -1:
+                s_p = 0
+            else:
+                s_p = env.player
+            aa_p = env.available_actions()
+
+            if env.is_game_over() or len(aa_p) == 0:
+                #Q[s_p, :] = 0.0
+                Q[s, (a[0] * 3 + a[1])] += alpha * (r - Q[s, (a[0] * 3 + a[1])])
+            else:
+                expected_value = np.dot(Q[s_p][[case[0] * 3 + case[1] for case in aa_p]], pi[s_p][[case[0] * 3 + case[1] for case in aa_p]])
+                Q[s, (a[0] * 3 + a[1])] += alpha * (r + gamma * expected_value - Q[s, (a[0] * 3 + a[1])])
+
+
+            #pi[s, :] = 0.0
+            pi[s, aa[np.argmax(Q[s][[case[0] * 3 + case[1] for case in aa]])]] = 1.0
+
+    return PolicyAndActionValueFunction(pi=dict(enumerate(pi)), q=dict(enumerate(Q)))
+
+
 def sarsa_on_secret_env3(
     gamma: float = 0.9999,
     alpha: float = 0.01,
@@ -739,9 +797,9 @@ def demo():
     print(expected_sarsa_on_grid_world(grid_world))
     """
 
-    print(sarsa_on_tic_tac_toe_solo(tictactoe))
+    # print(sarsa_on_tic_tac_toe_solo(tictactoe))
     # print(q_learning_on_tic_tac_toe_solo(tictactoe))
-    # print(expected_sarsa_on_tic_tac_toe_solo(tictactoe))
+    print(expected_sarsa_on_tic_tac_toe_solo(tictactoe))
 
     # print(sarsa_on_secret_env3())
     # print(q_learning_on_secret_env3())
