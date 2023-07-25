1310f04be63388999bcf56f26f1d3cfe6fe9c56c
ErwanDuprey <erwan.duprey@free.fr>
2023-07-25 14:33:11 +0200

    Interface for line and grid world for temporal_difference_learning.py

--- envs/interface_line_world_temporal_difference_learning.py
+++ envs/interface_line_world_temporal_difference_learning.py
@@ -1,28 +1,24 @@
-import sys
-import math
-import itertools
-import time
 import pygame
-from algorithms.temporal_difference_learning import (sarsa_on_tic_tac_toe_solo,
-                                                    q_learning_on_tic_tac_toe_solo,
-                                                    expected_sarsa_on_tic_tac_toe_solo)
-from envs.tictactoe import TicTacToe
+import time
+import sys
+import numpy as np
+from algorithms.temporal_difference_learning import (
+    sarsa_on_line_world,
+    q_learning_on_line_world,
+    expected_sarsa_on_line_world,
+)
+from envs.line_world import LineWorldEnv
 
-# Configuration de base de la fenêtre
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+row_len = 7
+column_len = 1
 BUTTON_WIDTH = 500
 BUTTON_HEIGHT = 70
 BUTTON_SPACING = 20
-LINE_WIDTH = 15
-BOARD_ROWS = 3
-BOARD_COLS = 3
-CIRCLE_RADIUS = 60
-CIRCLE_WIDTH = 15
-CROSS_WIDTH = 25
-SPACE = 55
+CELL_WIDTH = WINDOW_WIDTH // row_len
+CELL_HEIGHT = WINDOW_HEIGHT // row_len
 
-# Couleurs RGB
 LIGHT_BLUE = (14, 41, 84)
 DARK_BLUE = (31, 110, 140)
 ORANGE = (132, 167, 161)
@@ -30,52 +26,61 @@ ORANGE = (132, 167, 161)
 BUTTON_COLOR = DARK_BLUE
 WHITE = (242, 234, 211)
 
-# Crée une surface de fenêtre
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("TicTacToe")
 
-
-def draw_lines():
-    # Dessine les lignes horizontales
-    pygame.draw.line(screen, WHITE, (0, 200), (600, 200), LINE_WIDTH)
-    pygame.draw.line(screen, WHITE, (0, 400), (600, 400), LINE_WIDTH)
-    # Dessine les lignes verticales
-    pygame.draw.line(screen, WHITE, (200, 0), (200, 600), LINE_WIDTH)
-    pygame.draw.line(screen, WHITE, (400, 0), (400, 600), LINE_WIDTH)
-
-
-def draw_figures(env):
-    for enum, cell in enumerate(env.board):
-        if cell == 1:
-            pygame.draw.circle(
-                screen,
-                DARK_BLUE,
-                (int(enum%3 * 200 + 100), int(int(math.floor(enum/3)) * 200 + 100)),
-                CIRCLE_RADIUS,
-                CIRCLE_WIDTH,
+def draw_grid():
+    for y in range(column_len):
+        for x in range(row_len):
+            rect = pygame.Rect(
+                x * CELL_WIDTH,
+                y * CELL_HEIGHT + (WINDOW_WIDTH // 2 - CELL_WIDTH // 2),
+                CELL_WIDTH,
+                CELL_HEIGHT,
             )
-        elif cell == -1:
-            pygame.draw.line(
-                screen,
-                DARK_BLUE,
-                (enum%3 * 200 + SPACE, int(math.floor(enum/3)) * 200 + 200 - SPACE),
-                (enum%3 * 200 + 200 - SPACE, int(math.floor(enum/3)) * 200 + SPACE),
-                CROSS_WIDTH,
-            )
-            pygame.draw.line(
-                screen,
-                DARK_BLUE,
-                (enum%3 * 200 + SPACE, int(math.floor(enum/3)) * 200 + SPACE),
-                (enum%3 * 200 + 200 - SPACE, int(math.floor(enum/3)) * 200 + 200 - SPACE),
-                CROSS_WIDTH,
-            )
+            pygame.draw.rect(window, WHITE, rect, 1)
+            draw_text_in_cell("Gagné", 0, row_len - 1)
+            draw_text_in_cell("Perdu", 0, 0)
 
 
-def restart(env):
-    screen.fill(LIGHT_BLUE)
-    draw_lines()
-    env.reset()
-    draw_figures(env)
+def draw_agent(agent_pos):
+    x = agent_pos % row_len
+    y = agent_pos // row_len
+    rect = pygame.Rect(
+        x * CELL_WIDTH,
+        y * CELL_HEIGHT + (WINDOW_WIDTH // 2 - CELL_WIDTH // 2),
+        CELL_WIDTH,
+        CELL_HEIGHT,
+    )
+    pygame.draw.rect(window, ORANGE, rect)
+
+
+def draw_game_over(is_win):
+    font = pygame.font.Font(None, 74)
+    if is_win:
+        text = font.render("Gagné", True, WHITE)
+    else:
+        text = font.render("Game Over", True, WHITE)
+    text_rect = text.get_rect()
+    text_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 4)
+    window.blit(text, text_rect)
+
+
+def draw_move_count(move_count):
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Moves: {move_count}", True, WHITE)
+    window.blit(text, (20, 20))
+
+
+def draw_button(screen, text, x, y, width, height, color):
+    pygame.draw.rect(screen, color, (x, y, width, height))
+    font = pygame.font.Font(None, 50)
+    text = font.render(text, 1, WHITE)
+    screen.blit(
+        text,
+        (
+            x + (width / 2 - text.get_width() / 2),
+            y + (height / 2 - text.get_height() / 2),
+        ),
+    )
 
 
 def bouble_button(name1, name2):
@@ -99,60 +104,77 @@ def bouble_button(name1, name2):
     )
 
 
-def draw_game_over(winer):
-    font = pygame.font.Font(None, 74)  # Choisissez la taille de la police ici.
-    if winer == -1:
-        text = font.render(
-            "Circle players won!", True, WHITE
-        )  # Affiche 'Gagné' si le joueur a gagné.
-    else:
-        text = font.render(
-            "Cross players won!", True, WHITE
-        )  # Affiche 'Game Over' si le joueur a perdu.
-    text_rect = text.get_rect()
-    text_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 4)
-    window.blit(text, text_rect)
+def draw_text_in_cell(text, row, column):
+    font = pygame.font.Font(None, 36)
+    rendered_text = font.render(text, True, WHITE)
+    text_rect = rendered_text.get_rect()
+    cell_width = WINDOW_WIDTH // row_len
+    cell_height = WINDOW_HEIGHT // column_len
+    x = column * cell_width + cell_width // 2
+    y = row * cell_height + cell_height // 2
+    text_rect.center = (x, y)
+    window.blit(rendered_text, text_rect)
 
 
-def draw_button(screen, text, x, y, width, height, color):
-    pygame.draw.rect(screen, color, (x, y, width, height))
-    font = pygame.font.Font(None, 50)
-    text = font.render(text, 1, WHITE)
-    screen.blit(
-        text,
-        (
-            x + (width / 2 - text.get_width() / 2),
-            y + (height / 2 - text.get_height() / 2),
-        ),
-    )
+def get_directions(probs, col_len):
+    directions = {i: 0 for i in range(col_len)}
+    print("directions : ", directions)
+
+    for i in range(1, col_len + 1):
+        neighbours = []
+
+        if i > 1:
+            neighbours.append(i - 1)
+        if i < col_len:
+            neighbours.append(i + 1)
+
+        if col_len in neighbours:
+            if col_len == i + 1:
+                directions[i - 1] = 1
+            else:
+                directions[i - 1] = 2
+            continue
+
+        if not neighbours:
+            continue
+
+        max_prob_neighbour = max(neighbours, key=probs.get)
+
+        if max_prob_neighbour == i - 1:
+            directions[i - 1] = 0
+        elif max_prob_neighbour == i + 1:
+            directions[i - 1] = 1
+    print("directions : ", directions)
+
+    return directions
 
 
 def play_game():
     pygame.init()
-    screen.fill(LIGHT_BLUE)
-    env = TicTacToe()
-    # Boucle principale
+    move_count = 0
+    env = LineWorldEnv(7)
     running = True
-    player = 1
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouseX = event.pos[0]  # Coordonnées en X
-                mouseY = event.pos[1]  # Coordonnées en Y
-                clicked_row = int(mouseY // 200)
-                clicked_col = int(mouseX // 200)
-                if env.board[clicked_row*3 + clicked_col] == 0:
-                    env.act_with_action_id((clicked_row*3 + clicked_col))
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and 0 in env.available_actions():
+                    env.step(0)
+                    move_count += 1
+                elif event.key == pygame.K_RIGHT and 1 in env.available_actions():
+                    env.step(1)
+                    move_count += 1
 
-        screen.fill(LIGHT_BLUE)
+        window.fill(LIGHT_BLUE)
         if not env.is_game_over():
-            draw_figures(env)
-            draw_lines()
-
+            draw_grid()
+            draw_agent(env.state_id())
+            draw_move_count(move_count)
         else:
-            draw_game_over(env.current_player)
+            draw_game_over(env.is_game_won())
+            draw_move_count(move_count)
             bouble_button("exit", "rejouer")
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -178,54 +200,72 @@ def play_game():
                             play_game()
         pygame.display.flip()
 
-        pygame.display.update()
-
 
 def play(env, policy):
     pygame.init()
-    screen.fill(LIGHT_BLUE)
     clock = pygame.time.Clock()
-    # Boucle principale
+    move_count = 0
+    env.reset()
     running = True
-    possibilities = list(itertools.product([-1, 0, 1], repeat=9))
-    dict_tictactoe = {}
-    env.reset_random()
-    for possibility, key in zip(possibilities, policy.keys()):
-        dict_tictactoe[possibility] = key
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         window.fill(LIGHT_BLUE)
-        if not env.is_game_over():
-            choices = policy[dict_tictactoe[tuple(list(env.board))]]
-            max_key = max(choices, key=choices.get)
-            if env.board[max_key] == 0:
-                env.act_with_action_id(max_key)
-                time.sleep(0.5)
 
-            draw_figures(env)
-            draw_lines()
+        if not env.is_game_over():
+            prev_state_id = env.state_id()
+            action = np.argmax(policy[env.state_id()])
+            env.step(action)
+
+            if env.state_id() == prev_state_id:
+                move_count += 2
+            else:
+                move_count += 1
+
+            time.sleep(0.5)
+            draw_grid()
+            draw_agent(env.state_id())
+            draw_move_count(move_count)
 
         else:
+            draw_grid()
+            draw_agent(env.state_id())
+            draw_move_count(move_count)
             time.sleep(0.5)
+
             window.fill(LIGHT_BLUE)
-            draw_game_over(env.current_player)
+
+            draw_game_over(env.is_game_won())
+            draw_move_count(move_count)
             bouble_button("exit", "rejouer")
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
-                    if WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2 < mouse_pos[0] < WINDOW_WIDTH / 2 + BUTTON_WIDTH / 2:
-                        if WINDOW_HEIGHT / 2 - BUTTON_HEIGHT< mouse_pos[1]< WINDOW_HEIGHT / 2:
+                    if (
+                        WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2
+                        < mouse_pos[0]
+                        < WINDOW_WIDTH / 2 + BUTTON_WIDTH / 2
+                    ):
+                        if (
+                            WINDOW_HEIGHT / 2 - BUTTON_HEIGHT
+                            < mouse_pos[1]
+                            < WINDOW_HEIGHT / 2
+                        ):
                             running = False
-                        elif WINDOW_HEIGHT / 2 + BUTTON_HEIGHT < mouse_pos[1] < WINDOW_HEIGHT / 2 + BUTTON_HEIGHT * 2:
+                        elif (
+                            WINDOW_HEIGHT / 2 + BUTTON_HEIGHT
+                            < mouse_pos[1]
+                            < WINDOW_HEIGHT / 2 + BUTTON_HEIGHT * 2
+                        ):
                             play(env, policy)
-        time.sleep(0.5)
+
         pygame.display.flip()
-        pygame.display.update()
         clock.tick(60)
+
     main_menu()
 
 
@@ -240,7 +280,7 @@ def main_menu():
 
         draw_button(
             window,
-            "Play with Sarsa",
+            "Play with sarsa",
             WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2,
             WINDOW_HEIGHT / 2 - BUTTON_HEIGHT * 2 - BUTTON_SPACING * 1.5,
             BUTTON_WIDTH,
@@ -249,7 +289,7 @@ def main_menu():
         )
         draw_button(
             window,
-            "Play with Q-learning",
+            "Play with q learning",
             WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2,
             WINDOW_HEIGHT / 2 - BUTTON_HEIGHT - BUTTON_SPACING * 0.5,
             BUTTON_WIDTH,
@@ -258,7 +298,7 @@ def main_menu():
         )
         draw_button(
             window,
-            "Play with Expected Sarsa",
+            "Play with expected sarsa",
             WINDOW_WIDTH / 2 - BUTTON_WIDTH / 2,
             WINDOW_HEIGHT / 2 + BUTTON_SPACING * 0.5,
             BUTTON_WIDTH,
@@ -276,7 +316,7 @@ def main_menu():
         )
 
         pygame.display.flip()
-        env = TicTacToe()
+        env = LineWorldEnv(row_len)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -292,21 +332,21 @@ def main_menu():
                         < mouse_pos[1]
                         < WINDOW_HEIGHT / 2 - BUTTON_HEIGHT - BUTTON_SPACING * 0.5
                     ):
-                        policy = sarsa_on_tic_tac_toe_solo(env).pi
+                        policy = sarsa_on_line_world(env).pi
                         play(env, policy)
                     elif (
                         WINDOW_HEIGHT / 2 - BUTTON_HEIGHT - BUTTON_SPACING * 0.5
                         < mouse_pos[1]
                         < WINDOW_HEIGHT / 2 + BUTTON_SPACING * 0.5
                     ):
-                        policy = q_learning_on_tic_tac_toe_solo(env).pi
+                        policy = q_learning_on_line_world(env).pi
                         play(env, policy)
                     elif (
                         WINDOW_HEIGHT / 2 + BUTTON_SPACING * 0.5
                         < mouse_pos[1]
                         < WINDOW_HEIGHT / 2 + BUTTON_HEIGHT + BUTTON_SPACING * 1.5
                     ):
-                        policy = expected_sarsa_on_tic_tac_toe_solo(env).pi
+                        policy = expected_sarsa_on_line_world(env).pi
                         play(env, policy)
                     elif (
                         WINDOW_HEIGHT / 2 + BUTTON_HEIGHT + BUTTON_SPACING * 1.5
